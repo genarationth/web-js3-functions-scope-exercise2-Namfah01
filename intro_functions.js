78e89b8c677edeb7fe94801ea7582e3211932ce2
Namfah01 <kanokwanbing09@gmail.com>
2024-05-14 15:51:41 +0700

    T_T

---
+++ intro_functions.js
@@ -0,0 +1,23 @@
+
+function even(number) {
+    if (number % 2 === 0) {
+        return "Even Number" ;
+    } else {
+        return "Odd Number";
+    }
+}
+
+    console.log(even(24));
+
+
+
+const even = number => number % 2 === 0 ? "Even Number" : "Odd Number";
+
+console.log(even(22));
+
+
+
+
+
+
+
